24eed3f37f1eade4e0d123b686532a759028e5de
默涵 张 <1761504368@qq.com>
2023-06-25 15:12:39 +0800

    第一次add

--- zmh/src/muyuan/zz.java
+++ zmh/src/muyuan/zz.java
@@ -1,4 +1,19 @@
 package muyuan;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class zz {
+    public static void main(String[] args) {
+        List<String> strings= Arrays.asList("abc","def","cfs","fds");
+
+        //1.返回符合条件的stream
+        Stream<String> stringStream=strings.stream().filter(s->"abc".equals(s));
+
+        //2.计算流符合条件的流数量
+        long count=stringStream.count();
+        System.out.println(count);
+    }
+
 }
